f50505d32323548f2d605ec523c1783812597657
Prax <xav.axp@gmail.com>
2016-07-24 16:43:24 +0530

    added server changes for importing models

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -1,12 +1,42 @@
 var express = require('express');
 var router = express.Router();
 var app = express();
+
+var bodyParser = require('body-parser');
+var Promise = require('bluebird');
+
 var pg = require('pg');
 var knex = require('../../../db/knex');
 var passport = require('../lib/passport');
 var bcrypt = require('bcrypt');
 var helpers = require('../lib/helpers');
 var bookshelf = require('bookshelf')(knex);
+
+//load models
+var Recipe = require('../../../db/models/Recipe');
+
+
+app.set('bookshelf', bookshelf);
+
+var allowCrossDomain = function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+};
+
+app.use(allowCrossDomain);
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded());
+
+// parse application/json
+app.use(bodyParser.json());
+
+// parse application/vnd.api+json as json
+app.use(bodyParser.json({type: 'application/vnd.api+json'}));
+
+
+//defining bookshel models
+
+
 
 app.use('/client', express.static(__dirname + '/client'));
 
@@ -18,12 +48,48 @@ router.get('/', function(req, res, next) {
 router.get('/list', function(req, res, next) {
   //returns a list
   //QUERY - select dishes from dishes_list
-  
+  new Recipe().fetchAll()
+    .then(function(articles) {
+      res.send(articles.toJSON());
+    }).catch(function(error) {
+      console.log(error);
+      res.send('An error occured');
+    });
 
+  var obj = { a: 1 };
+  var result = JSON.stringify(obj);
 
-  res.render('index', { title: 'Eatables' });
+  // res.render('index', { title: 'Eatables' });
+  // res.send(result);
 });
 
+///documents/:format/:type
+router.get('/recipe/:title/:content', function(req, res, next) {
+  //returns a list
+  //QUERY - select dishes from dishes_list
+  // new Recipe().fetchAll()
+  //   .then(function(articles) {
+  //     res.send(articles.toJSON());
+  //   }).catch(function(error) {
+  //     console.log(error);
+  //     res.send('An error occured');
+  //   });
+
+  var format = req.params.title,
+       type = req.params.content;
+
+  var obj = { a: format,b: type };
+  var result = JSON.stringify(obj);
+
+  // knex('recipe').insert({title: format,content:type})
+
+  new Recipe({title: format,content:type}).save().then(function(model) {
+   console.log(model);
+  });
+
+  // res.render('index', { title: 'Eatables' });
+  res.send(result);
+});
 
 
 module.exports = router;
